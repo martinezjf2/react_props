4dedf16bdedc1e9be5d88e0a8503ad9d0c8da0ec
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-26 17:59:39 -0500

    refractory with props

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Card() {
+function Card(props) {
   return (
     <div>
-      <h1>Artists</h1>
-
-      <h2>Beyonce</h2>
-      <img
-        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        alt="avatar_img"
-      />
-      <p>+123 456 789</p>
-      <p>b@beyonce.com</p>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.phone}</p>
+      <p>{props.email}</p>
 
       <h2>Jack Bauer</h2>
       <img
@@ -28,6 +23,12 @@ function Card() {
 ReactDOM.render(
   <div>
     <h1>Artists</h1>
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      phone="+123 456 789"
+      email="b@beyonce.com"
+    />
   </div>,
   document.getElementById("root")
 );
