d9c6009389ace1114ddfd1791eadf9b46f633bb6
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-26 18:04:01 -0500

    created another card

--- src/index.js
+++ src/index.js
@@ -9,13 +9,10 @@ function Card(props) {
       <p>{props.phone}</p>
       <p>{props.email}</p>
 
-      <h2>Jack Bauer</h2>
-      <img
-        src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-        alt="avatar_img"
-      />
-      <p>+987 654 321</p>
-      <p>jack@nowhere.com</p>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.phone}</p>
+      <p>{props.email}</p>
     </div>
   );
 }
@@ -28,6 +25,12 @@ ReactDOM.render(
       img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
       phone="+123 456 789"
       email="b@beyonce.com"
+    />
+    <Card
+      name="Jack Bauer"
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      phone="+987 654 321"
+      email="jack@nowhere.com"
     />
   </div>,
   document.getElementById("root")
