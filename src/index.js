1a208a1a85d021756e09c312ee3b50787e2c88d7
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-26 18:05:58 -0500

    did not need to duplicate lines 6-10 because it getting all data from properties

--- src/index.js
+++ src/index.js
@@ -8,11 +8,6 @@ function Card(props) {
       <img src={props.img} alt="avatar_img" />
       <p>{props.phone}</p>
       <p>{props.email}</p>
-
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="avatar_img" />
-      <p>{props.phone}</p>
-      <p>{props.email}</p>
     </div>
   );
 }
